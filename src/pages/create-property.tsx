6a18c77157bdc8622bb5aeee0db8c62e4f9bf83d
tony l <33114223+Tony9Lee@users.noreply.github.com>
2023-03-09 02:01:19 -0800

    Day3

--- src/pages/create-property.tsx
+++ src/pages/create-property.tsx
@@ -1,7 +1,27 @@
 import { useState } from "react";
+import { useGetIdentity } from "@pankod/refine-core";
+//useGetIdentity hook fetches and manages user-related data from the backend
 
-const createProperty = () => {
+import { FieldValues, useForm } from "@pankod/refine-react-hook-form";
+//useForm returns 'methods' like 'handleSubmit' to handle form submissions, 'register'to register form inputs, & 'reset' to reset the form
+//Fieldvalues represents the values to be stored and ensures type safety when working with form data
+
+import { useNavigate } from "@pankod/refine-react-router-v6";
+//Accesses navigate function and calls it with a route string or location object to navigate to desired location within application
+
+import Form from "components/common/Form";
+
+const CreateProperty = () => {
+  const navigate = useNavigate();
+  const { data: user } = useGetIdentity();
+  const [propertyImage, setPropertyImage] = useState({ name: "", url: "" });
+  const {
+    refineCore: { onFinish, formLoading },
+    register,
+    handleSubmit,
+  } = useForm();
+
   return <div>create-property</div>;
 };
 
-export default createProperty;
+export default CreateProperty;
