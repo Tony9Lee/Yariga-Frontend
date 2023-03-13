24496b2fc780df5c6e316626b9649d16b9b698e9
tony l <33114223+Tony9Lee@users.noreply.github.com>
2023-03-12 19:50:01 -0700

    Day4

--- src/App.tsx
+++ src/App.tsx
@@ -133,7 +133,7 @@ function App() {
         <GlobalStyles styles={{ html: { WebkitFontSmoothing: "auto" } }} />
         <RefineSnackbarProvider>
           <Refine
-            dataProvider={dataProvider("https://localhost:8080/api/v1")}
+            dataProvider={dataProvider("http://localhost:8080/api/v1")}
             notificationProvider={notificationProvider}
             ReadyPage={ReadyPage}
             catchAll={<ErrorComponent />}
--- src/pages/all-properties.tsx
+++ src/pages/all-properties.tsx
@@ -1,6 +1,6 @@
 import { Add } from "@mui/icons-material";
 // "Add" Icon from MUI
-import { useList } from "@pankod/refine-core";
+import { useTable } from "@pankod/refine-core";
 // Hook that fetches list of resources or data from external API
 import { Box, Stack, Typography } from "@pankod/refine-mui";
 import { useNavigate } from "@pankod/refine-react-router-v6";
@@ -9,6 +9,18 @@ import { PropertyCard, CustomButton } from "components";
 
 const AllProperties = () => {
   const navigate = useNavigate();
+  
+  const {
+    tableQueryResult: { data, isLoading, isError }
+  } = useTable();
+
+  const allProperties = data?.data ?? [];
+
+  if(isLoading) return <Typography>Loading...</Typography>
+  if(isError) return <Typography>Loading...</Typography>
+
+
+
   return (
     <Box>
       <Stack direction="row" justifyContent="space-between" alignItems="center">
@@ -23,6 +35,18 @@ const AllProperties = () => {
           icon={<Add />}
         />
       </Stack>
+      <Box mt="20px" sx={{ display: "flex", flexWrap: "wrap", gap: 3 }}>
+        {allProperties.map((property) => (
+          <PropertyCard
+            key={property._id}
+            id={property._id}
+            title={property.title}
+            location={property.location}
+            price={property.price}
+            photo={property.photo}
+            />
+        ))}
+      </Box>
     </Box>
   );
 };
